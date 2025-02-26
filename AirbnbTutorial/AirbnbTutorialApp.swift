3de6bfcceaaf57ba2dad22cab02dde1cb7933d7c
ijuvrk <ijascochin@gmail.com>
2025-02-26 19:58:13 +0300

    Initial Commit

---
+++ AirbnbTutorial/AirbnbTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirbnbTutorialApp.swift
+//  AirbnbTutorial
+//
+//  Created by Ijaz Ahmad on 26/02/25.
+//
+
+import SwiftUI
+
+@main
+struct AirbnbTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
