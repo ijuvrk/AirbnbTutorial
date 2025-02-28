b5ae2abfe86004dc7d1016e3098ca178f53fe88e
ijuvrk <ijascochin@gmail.com>
2025-02-28 23:11:46 +0300

    created different views renamed ListingView into ListingItemView

---
+++ AirbnbTutorial/Core/Explore/ExploreView.swift
@@ -0,0 +1,18 @@
+//
+//  ExploreView.swift
+//  AirbnbTutorial
+//
+//  Created by Ijaz Ahmad on 27/02/25.
+//
+
+import SwiftUI
+
+struct ExploreView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ExploreView()
+}
---
+++ AirbnbTutorial/Core/Listing/ListingItemView.swift
@@ -0,0 +1,18 @@
+//
+//  ListingView.swift
+//  AirbnbTutorial
+//
+//  Created by Ijaz Ahmad on 28/02/25.
+//
+
+import SwiftUI
+
+struct ListingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ListingItemView()
+}
