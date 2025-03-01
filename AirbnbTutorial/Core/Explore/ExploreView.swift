701f4e0106e494de03982f71f2ae715e790e3fff
ijuvrk <ijascochin@gmail.com>
2025-03-01 23:02:12 +0300

    added search and filter bar on top of the home view

--- AirbnbTutorial/Core/Explore/ExploreView.swift
+++ AirbnbTutorial/Core/Explore/ExploreView.swift
@@ -9,7 +9,17 @@ import SwiftUI
 
 struct ExploreView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            ScrollView {
+                SearchAndFilterBar()
+                LazyVStack(spacing: 32) {
+                    ForEach(0 ... 10, id: \.self) { listing in
+                        ListingItemView()
+                            .clipShape(RoundedRectangle(cornerRadius: 10))
+                    }
+                }
+            }
+        }
     }
 }
 
--- AirbnbTutorial/Core/Listing/ListingItemView.swift
+++ AirbnbTutorial/Core/Listing/ListingItemView.swift
@@ -1,5 +1,5 @@
 //
-//  ListingView.swift
+//  ListingItemView.swift
 //  AirbnbTutorial
 //
 //  Created by Ijaz Ahmad on 28/02/25.
@@ -7,12 +7,60 @@
 
 import SwiftUI
 
-struct ListingView: View {
+struct ListingItemView: View {
+    
+    var images = [
+        "listing-1",
+        "listing-2",
+        "listing-3",
+        "listing-4",
+    ]
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 8) {
+            // Images
+            TabView {
+                ForEach(images, id: \.self) { image in
+                    Image(image)
+                        .resizable()
+                }
+            }
+            .frame(height: 320)
+            .clipShape(RoundedRectangle(cornerRadius: 10))
+            .tabViewStyle(.page)
+                
+                // listing details
+                HStack(alignment: .top) {
+                    // details
+                    VStack(alignment: .leading) {
+                        Text("Miami, Florida")
+                            .fontWeight(.semibold)
+                        Text("12 mi away")
+                            .foregroundStyle(.gray)
+                        Text("Nov 3 - 25")
+                            .foregroundStyle(.gray)
+                        HStack(spacing: 4) {
+                            Text("$567")
+                                .fontWeight(.semibold)
+                            Text("night")
+                        }
+                    }
+                    
+                    Spacer()
+                    // rating
+                    HStack(spacing: 2) {
+                        Image(systemName: "star.fill")
+                        Text("4.86")
+                    }
+                }
+                .font(.footnote)
+                
+            }
+            .padding()
+        }
     }
-}
 
-#Preview {
-    ListingItemView()
-}
+    #Preview {
+        ListingItemView()
+    }
+
