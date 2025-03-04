022f85c8010c36319d2677a1fb8d983520f34ac7
ijuvrk <ijascochin@gmail.com>
2025-03-04 22:45:23 +0300

    used @State property to navigate to the view inside of the searchandfilterbar. it gives precise control over view appearance and more custom transitions.

---
+++ AirbnbTutorial/Core/Explore/DestinationSearchView.swift
@@ -0,0 +1,18 @@
+//
+//  DestinationSearchView.swift
+//  AirbnbTutorial
+//
+//  Created by Ijaz Ahmad on 04/03/25.
+//
+
+import SwiftUI
+
+struct DestinationSearchView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DestinationSearchView()
+}
