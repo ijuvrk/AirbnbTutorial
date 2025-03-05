861af5e680191aff405c6320748e608ad88b64b2
ijuvrk <ijascochin@gmail.com>
2025-03-05 21:41:55 +0300

    state and binding navigated to destinationSearchView using state created a custom close button using @Binding to go back to exploreview created where to section in the DestinationSearchView implemented text input field with binding

--- AirbnbTutorial/Core/Explore/DestinationSearchView.swift
+++ AirbnbTutorial/Core/Explore/DestinationSearchView.swift
@@ -8,11 +8,51 @@
 import SwiftUI
 
 struct DestinationSearchView: View {
+    @Binding var show: Bool
+    @State private var destination = ""
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            
+            Button {
+                withAnimation(.snappy) {
+                    show.toggle() // toggles state value and returns to ExploreView.
+                }
+            } label: {
+                Image(systemName: "xmark.circle")
+                    .imageScale(.large)
+                    .foregroundStyle(.black)
+            }
+            
+            VStack(alignment: .leading) {
+                Text("Where to?")
+                    .font(.title2)
+                    .fontWeight(.semibold)
+                
+                HStack {
+                    Image(systemName: "magnifyingglass")
+                        .imageScale(.small)
+                    TextField("Search destinations", text: $destination)
+                        .font(.subheadline)//binds to the state. and stores in the state variable. state changed -> view refreshes, text is shown
+                }
+                .frame(height: 44)
+                .padding(.horizontal)
+                .overlay {
+                    RoundedRectangle(cornerRadius: 8)
+                        .stroke(lineWidth: 1.0)
+                        .foregroundStyle(Color(.systemGray4))
+                }
+            }
+            .padding()
+            .background(.white)
+            .clipShape(RoundedRectangle(cornerRadius: 12))
+            .shadow(radius: 10)
+            .padding()
+            
+        }
     }
 }
 
 #Preview {
-    DestinationSearchView()
+    DestinationSearchView(show: .constant(false)) //just for preview, dummy binding since there is no parentview while on preview mode
 }
--- AirbnbTutorial/Core/Explore/ExploreView.swift
+++ AirbnbTutorial/Core/Explore/ExploreView.swift
@@ -8,27 +8,40 @@
 import SwiftUI
 
 struct ExploreView: View {
+    @State private var showDestinationSearchView = false //gives control to create custom animations, precise control over view switching
+    
     var body: some View {
         NavigationStack {
-            ScrollView {
-                SearchAndFilterBar()
-                LazyVStack(spacing: 32) {
-                    ForEach(0 ... 10, id: \.self) { listing in
-                        NavigationLink(value: listing) {
-                            ListingItemView()
-                                .frame(height: 400)
-                                .clipShape(RoundedRectangle(cornerRadius: 10))
+            
+            if showDestinationSearchView {
+                DestinationSearchView(show: $showDestinationSearchView) //$ creates a binding with state. "show" is binding variable.
+                //binding is required
+            } else {
+                ScrollView {
+                    SearchAndFilterBar()
+                        .onTapGesture {
+                            withAnimation(.snappy) {
+                                showDestinationSearchView.toggle()
+                            }
+                        }
+                    LazyVStack(spacing: 32) {
+                        ForEach(0 ... 10, id: \.self) { listing in
+                            NavigationLink(value: listing) {
+                                ListingItemView()
+                                    .frame(height: 400)
+                                    .clipShape(RoundedRectangle(cornerRadius: 10))
+                            }
                         }
                     }
+                    .padding()
                 }
-                .padding()
+                .navigationDestination(for: Int.self) { listing in
+                    ListingDetailView()
+                        .navigationBarBackButtonHidden()
+                        .toolbar(.hidden)
+                }
             }
-            .navigationDestination(for: Int.self) { listing in
-                ListingDetailView()
-                    .navigationBarBackButtonHidden()
-                    .toolbar(.hidden)
             }
-        }
     }
 }
 
