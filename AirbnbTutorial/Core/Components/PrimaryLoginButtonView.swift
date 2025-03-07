9f3fe039db5a00cb4e6273a16cbbffac21b0ab34
ijuvrk <ijascochin@gmail.com>
2025-03-07 22:26:00 +0300

    extracted the login button into a separate view for reusing

---
+++ AirbnbTutorial/Core/Components/PrimaryLoginButtonView.swift
@@ -0,0 +1,18 @@
+//
+//  PrimaryLoginButtonView.swift
+//  AirbnbTutorial
+//
+//  Created by Ijaz Ahmad on 07/03/25.
+//
+
+import SwiftUI
+
+struct PrimaryLoginButtonView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PrimaryLoginButtonView()
+}
---
+++ AirbnbTutorial/Core/WishList/WishlistView.swift
@@ -0,0 +1,18 @@
+//
+//  WishlistView.swift
+//  AirbnbTutorial
+//
+//  Created by Ijaz Ahmad on 07/03/25.
+//
+
+import SwiftUI
+
+struct WishlistView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WishlistView()
+}
