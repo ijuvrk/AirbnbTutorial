7d222c7a25cab1615bc850765c1306026a9219be
ijuvrk <ijascochin@gmail.com>
2025-03-07 21:57:17 +0300

    completed profile view UI

---
+++ AirbnbTutorial/Core/Profile/ProfileOptionRowView.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileOptionRowView.swift
+//  AirbnbTutorial
+//
+//  Created by Ijaz Ahmad on 07/03/25.
+//
+
+import SwiftUI
+
+struct ProfileOptionRowView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProfileOptionRowView()
+}
---
+++ AirbnbTutorial/Core/Profile/ProfileView.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileView.swift
+//  AirbnbTutorial
+//
+//  Created by Ijaz Ahmad on 07/03/25.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProfileView()
+}
