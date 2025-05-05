2ef487be5e9516f6f6018d52687692d02d89c6f7
ijuvrk <ijascochin@gmail.com>
2025-05-05 23:45:41 +0300

    connectedTextField

--- AirbnbTutorial/Core/Components/ConnectedTextField.swift
+++ AirbnbTutorial/Core/Components/ConnectedTextField.swift
@@ -11,14 +11,17 @@ struct ConnectedTextField: View {
     @State private var text = ""
     @State private var text2 = ""
     
+    let header = ""
+    let header2 = ""
     let placeholder = ""
     
     var body: some View {
         VStack {
+            Text(header)
             TextField(placeholder, text: $text)
             
             Divider()
-            
+            Text(header2)
             TextField(placeholder, text: $text2)
         }
     }
