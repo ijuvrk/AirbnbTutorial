1ea721ec48dbc2ba18b477655e555a98f797f467
ijuvrk <ijascochin@gmail.com>
2025-05-25 13:07:02 +0300

    Feat: creating connectedTextField which can be used to create as many fields inside
    - can give divider according to the number of fields
    - make secureField by conditional statement

--- AirbnbTutorial/Core/Components/ConnectedTextField.swift
+++ AirbnbTutorial/Core/Components/ConnectedTextField.swift
@@ -8,29 +8,58 @@
 import SwiftUI
 
 struct ConnectedTextField: View {
-    @State private var text = ""
-    @State private var text2 = ""
     
-    let header = ""
-    let header2 = ""
-    let placeholder = ""
+    let fields: [FieldData]
     
     var body: some View {
-        VStack {
-            Text(header)
-            TextField(placeholder, text: $text)
-            
-            Divider()
-            
-            Text(header2)
-            TextField(placeholder, text: $text2)
-        }
-        .padding(10)
-        .overlay {
-            RoundedRectangle(cornerRadius: 5)
-                .stroke(Color.gray, lineWidth: 0.2)
-        }
+        
     }
+    
+    
+
+    
+    
+    
+//    @State private var text = ""
+//    @State private var text2 = ""
+//    
+//    let header = ""
+//    let header2 = ""
+//    let placeholder = ""
+//    
+//    var body: some View {
+//        VStack {
+//            Text(header)
+//                .foregroundStyle(.secondary)
+//            TextField(placeholder, text: $text)
+//                .keyboardType(.emailAddress)
+//            
+//            Divider()
+//                .frame(height: 0.5)
+//                .background(Color.primary.opacity(0.2))
+//                .padding(.horizontal, -10)
+//            
+//            Text(header2)
+//                .foregroundStyle(.secondary)
+//            SecureField(placeholder, text: $text2)
+//        }
+//        .padding(10)
+//        .overlay {
+//            RoundedRectangle(cornerRadius: 5)
+//                .stroke(Color.primary.opacity(0.5), lineWidth: 0.2)
+//        }
+//        .padding(10)
+//    }
+}
+
+struct FieldData {
+    let label: String
+    let placeholder: String
+    let text: Binding<String>
+    let isSecure: Bool
+    let keyboardType: UIKeyboardType
+    let capitalization: TextInputAutocapitalization
+    let disableAutoCorrection: Bool
 }
 
 #Preview {
