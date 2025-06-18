d4850960597fb36bfceee7975f5f32a7f218f869
ijuvrk <ijascochin@gmail.com>
2025-06-18 23:37:11 +0300

    feat: implemented custom textfield and reusable signup button in signupview
    - added ConnectedTextField to handle first and last name
    - used PrimaryLoginButtonView component for the signup button

--- AirbnbTutorial/Core/Authentication/View/SignUpView.swift
+++ AirbnbTutorial/Core/Authentication/View/SignUpView.swift
@@ -13,31 +13,40 @@ struct SignUpView: View {
     @State private var firstName = ""
     @State private var secondName = ""
     @State private var confirmPassword = ""
-    
+
     var body: some View {
-        
+
         VStack(alignment: .leading, spacing: 8) {
             Image("airbnb-1")
                 .resizable()
                 .scaledToFit()
                 .frame(width: 40, height: 80)
-            
+
             Text("Sign up")
                 .font(.title2)
                 .fontWeight(.medium)
                 .kerning(0.9)
                 .padding(.bottom, 15)
-            
-            VStack(spacing: 0) {
-                CustomTextField(placeholder: "First Name", isSecure: false, text: $firstName)
-                Divider()
-                CustomTextField(placeholder: "Second Name", isSecure: false, text: $secondName)
-            }
-            
-            ConnectedTextField(fields: [FieldData(label: "First Name", placeholder: "First Name", text: $firstName),
-                                        FieldData(label: "Second Name", placeholder: "Second Name", text: $secondName)
-                                       ])
-            
+
+            ConnectedTextField(fields: [
+                FieldData(
+                    label: "First Name",
+                    placeholder: "First Name",
+                    text: $firstName
+                ),
+                FieldData(
+                    label: "Second Name",
+                    placeholder: "Second Name",
+                    text: $secondName
+                ),
+            ])
+
+            PrimaryLoginButtonView(
+                action: { print("button clicked") },
+                buttonText: "SignUp"
+            )
+            .padding(.top, 24)
+
             Spacer()
         }
         .padding(.horizontal)
